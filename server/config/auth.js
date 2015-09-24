b732b64b29f4b732edcdce3cd2eb4b4137963425
Michael Wetherald <Compewter@users.noreply.github.com>
2015-09-23 19:41:13 -0700

    Delete auth.js

--- server/config/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-  'googleAuth': {
-    clientID : "672763725891-3o9gnkibvi3q61ivotrrfil5e4fpbr1b.apps.googleusercontent.com",
-    clientSecret: "tAxro1l9z4zWqGouX5wFoufX",
-    callbackURL: "http://localhost:8080/auth/google/callback"
-  }
-
-};
