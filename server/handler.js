24ac790066016e49ffd042f708e9c252e3e85e09
Ryan Wholey <rjwholey@gmail.com>
2015-09-24 16:13:43 -0700

    added api routing to database, not working due to authentication issues

--- server/handler.js
+++ server/handler.js
@@ -1,4 +1,5 @@
 var passport = require('passport');
+var query = require('../db/queryHandler.js');
 
 module.exports = {
 
@@ -6,11 +7,21 @@ module.exports = {
 	users : {
 		get : function (req, res, next) {
 			console.log('user get');
-			res.status(200).send({});
+			res.status(200);
+			query.getUsers()
+				.then(function(data){
+					res.send(data);
+				})
+				.catch(function(err){
+					console.error('Get users error: ', err);
+				});
 		},
 		post : function (req, res, next)	{
+			// authentication takes care of all user posting to db
 			console.log('user post');
-			res.status(201).send('user posted.');
+			res.status(300);
+			res.send('user posting handled by Oauth')
+			// res.redirect('/');
 		}
 	},
 	
@@ -18,11 +29,26 @@ module.exports = {
 	challenges : {
 		get : function (req, res, next) {
 			console.log('challenges get');
-			res.status(200).send({});
+			res.status(200);
+			query.getChallenges()
+				.then(function(data){
+					res.send(data);
+				})
+				.catch(function(err){
+					console.log('Get challenges error: ', err);
+				});
+
 		},
 		post : function (req, res, next) {
 			console.log('challenges post');
-			res.status(201).send('challenge posted');
+			res.status(201);
+			query.createChallenge(req.body)
+				.then(function(){
+					res.send('Challenge posted successfully!')
+				})
+				.catch(function(err){
+					res.send('Challenge failed to post: ', err);
+				});
 		}
 	}
 
