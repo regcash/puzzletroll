1a69274349422ad7d1e0cbe13cd3258ede5ecd85
Sebastian Covarrubias <Seb@Sebastians-MacBook-Air.local>
2015-09-25 16:20:41 -0700

    Making userView and userController.

--- client/app.js
+++ client/app.js
@@ -1,9 +1,13 @@
-angular.module('puzzleTroll', ['ui.router', 'puzzleTroll.listModule'])
+angular.module('puzzleTroll', ['ui.router', 'puzzleTroll.listModule', 'puzzleTroll.userModule'])
 	.config(function ($stateProvider, $urlRouterProvider) {
 		// $urlRouterProvider.otherwise('/login');
 		$stateProvider
       .state('home', {
         url: '/',
         templateUrl: 'challengeList/listView.html'
+      })
+      .state('user', {
+        url: '/:user',
+        templateUrl: 'user/userView.html'
       });
 	});
