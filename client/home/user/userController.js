ca1122ba36712a3e81147abe068c48c923bbda28
Brian Graham <graham.briank@gmail.com>
2015-09-29 13:12:55 -0700

    add semicolons in userController, stub out challengeView, fix typo that prevented challengeController from loading

--- client/home/user/userController.js
+++ client/home/user/userController.js
@@ -4,12 +4,11 @@ angular.module('puzzleTroll.userModule', ['ui.router', 'puzzleTroll.Util'])
 	console.log($stateParams.user);
 	reqUtil.get('users', $stateParams.user)
 		.then(function(data){
-			var user = data.data
+			var user = data.data;
 			$scope.name = user.name;
 			$scope.email = user.email;
 		})
 		.catch(function(err){
 			console.error(err);
-		})
-
-}])
+		});
+}]);
