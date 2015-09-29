57989e9b86713ef513f77030788916f1cd5472ac
Sebastian Covarrubias <Seb@Sebastians-MacBook-Air.local>
2015-09-29 13:19:42 -0700

    Minor changes.

--- client/home/user/userController.js
+++ client/home/user/userController.js
@@ -1,6 +1,6 @@
-angular.module('puzzleTroll.userModule', [])
-.controller('userController', function ($scope) {
-  $scope.name;
+angular.module('puzzleTroll.userModule', ['puzzleTroll.Util'])
+.controller('userController', function ($scope, http) {
+  $scope.name = http.get('users');
   $scope.email;
   $scope.completedChallenges;
   $scope.authoredChallenges;
