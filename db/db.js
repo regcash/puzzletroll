0029369cdef10646d6a43247400d8170359dcb30
Brian Graham <graham.briank@gmail.com>
2015-10-05 10:45:35 -0700

    modify db connection

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('mysql://b96749e1b4baf1:5bc97a68@us-cdbr-iron-east-03.cleardb.net/heroku_468f49dcc4283fd?reconnect=true', 'root', '', {
+var sequelize = new Sequelize('mysql://b96749e1b4baf1:5bc97a68@us-cdbr-iron-east-03.cleardb.net/heroku_468f49dcc4283fd?reconnect=true', {
   define: {
     timestamps: false
     }
