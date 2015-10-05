110685461eddfb6df9ebb9c4a97930f2d78c36c4
Brian Graham <graham.briank@gmail.com>
2015-10-05 12:29:09 -0700

    more deploy changes, change localhost

--- server/config/auth.js
+++ server/config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
   'googleAuth': {
     clientID : "672763725891-e616nmshtg7sks16tosf8pe2k2anqeqp.apps.googleusercontent.com",
     clientSecret: "rNGNvWiMNYw13Y96F6Av5nlo",
-    callbackURL: "http://localhost:8080/auth/google/callback"
+    callbackURL: "https://fast-scrubland-2219.herokuapp.com/auth/google/callback"
   }
 
 };
