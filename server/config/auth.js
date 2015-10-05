40d7c4d489fa76c83d6727d00c84d65cc7216797
Brian Graham <graham.briank@gmail.com>
2015-10-05 10:26:28 -0700

    Add deployment JS file

---
+++ server/config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+  'googleAuth': {
+    clientID : "672763725891-e616nmshtg7sks16tosf8pe2k2anqeqp.apps.googleusercontent.com",
+    clientSecret: "rNGNvWiMNYw13Y96F6Av5nlo",
+    callbackURL: "http://localhost:8080/auth/google/callback"
+  }
+
+};
