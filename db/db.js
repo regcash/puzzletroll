ecebe9dfa8573863b8a35dbca64b7c7e15ccd30b
Brian Graham <graham.briank@gmail.com>
2015-10-05 11:18:36 -0700

    more modding of the db

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize(process.env.DATABASE_URL, "b96749e1b4baf1", "5bc97a68", {
+var sequelize = new Sequelize(process.env.DATABASE_URL, {
   define: {
     timestamps: false
     }
