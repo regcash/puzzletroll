8904b2085618c96f8798838d8b97453c01cb3d21
Brian Graham <graham.briank@gmail.com>
2015-10-05 15:51:30 -0700

    Delete auth.js

--- server/config/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-  'googleAuth': {
-    clientID : "672763725891-e616nmshtg7sks16tosf8pe2k2anqeqp.apps.googleusercontent.com",
-    clientSecret: "rNGNvWiMNYw13Y96F6Av5nlo",
-    callbackURL: "https://fast-scrubland-2219.herokuapp.com/auth/google/callback"
-  }
-
-};
